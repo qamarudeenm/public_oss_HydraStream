70760c1ce32ad35ce2f6f5eefbc8ca2225001b4e
Qamarudeen Muhammad <qamarudeen.m@gmail.com>
2026-04-02 22:14:08 +0100

    fix missing product metdat across all service

--- dbt/models/clickstream_summary.sql
+++ dbt/models/clickstream_summary.sql
@@ -2,7 +2,7 @@
     materialized='streaming_table',
     with={
         'connector': 'jdbc',
-        'url': 'jdbc:postgresql://host.docker.internal:5414/postgres?currentSchema=clickstream',
+        'url': 'jdbc:postgresql://localhost:5414/postgres?currentSchema=clickstream',
         'table-name': 'clickstream_analytics',
         'driver': 'org.postgresql.Driver',
         'username': 'data_eng',
@@ -13,10 +13,10 @@
 SELECT 
     event_type,
     session_id,
-    product_id,
+    COALESCE(product_id, `data`['product_id']) as product_id,
     COUNT(*) as event_count,
     window_start,
     window_end
 FROM TABLE(
     TUMBLE(TABLE {{ ref('clickstream_raw') }}, DESCRIPTOR(event_time), INTERVAL '1' MINUTE))
-GROUP BY event_type, session_id, product_id, window_start, window_end
+GROUP BY event_type, session_id, COALESCE(product_id, `data`['product_id']), window_start, window_end
